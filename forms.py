6ddcc6854bc38c579dbd183662ac961b7cef045c
Allinson Matthew <mrallinson@gmail.com>
2021-08-05 22:27:32 +0300

    rearrange form

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.fields.html5 import DecimalRangeField
 
 class ParameterForm(FlaskForm):
-	prefix = StringField('Prefix (optional)')
+	prefix = StringField('Prefix')
 	temperature = DecimalRangeField('Temperature', 
 		default=1.0)
 	submit = SubmitField('Make village names')
