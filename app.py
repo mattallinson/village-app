e2f4a615b82553312500c4ff50636dc5c55a012c
Allinson Matthew <mrallinson@gmail.com>
2021-08-30 11:43:25 +0300

    boosts connection limit

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def clear_list():
 	return redirect(url_for('index'))
 
 if __name__ == '__main__':
-	serve(app, host='0.0.0.0', port=80)
+	serve(app, host='0.0.0.0', port=80, connection_limit=500)
